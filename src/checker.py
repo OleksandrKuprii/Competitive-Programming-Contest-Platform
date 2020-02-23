d3d528813f27bbb92212ed5f827679fb5bad0f03
SashaKuprii <sasha2017.kuprii@gmail.com>
2020-02-23 14:38:42 +0200

    Updated checker

--- src/checker.py
+++ src/checker.py
@@ -1,21 +1,23 @@
-from dataclass import ResultToDB
+import asyncio
+
+from src.dataclass import ResultToDB
+
+from src.storage import get_correct_results
+from src.database import get_points_for_tests, add_results_to_db, change_submission_status
 
 
-from storage import get_correct_results
-
-
-def get_result(result_to_checker):
+async def get_result(result_to_checker):
+    submission_id = result_to_checker.submission_id
     test_ids = [x.test_id for x in result_to_checker.test_results]
     correct_results = get_correct_results(test_ids)
+    points = await get_points_for_tests(test_ids)
+    result_to_db = check_result(result_to_checker.test_results, correct_results, submission_id, points)
 
-    result_to_db = check_result(
-        result_to_checker.test_results,
-        correct_results, result_to_checker.submission_id)
-
-    return result_to_db
+    await add_results_to_db(result_to_db)
+    await change_submission_status(submission_id, 'Completed')
 
 
-def check_result(test_results, correct_results, submission_id):
+def check_result(test_results, correct_results, submission_id, points):
     def check_one(output, right):
         output = output.strip()
         right = right.strip()
@@ -26,13 +28,13 @@ def check_result(test_results, correct_results, submission_id):
 
     result_to_db = []
 
-    for test_result, correct in zip(test_results, correct_results):
+    for test_result, correct, point in zip(test_results, correct_results, points):
         test_id = test_result.test_id
         status = test_result.status
         result = test_result.result
         wall_time = test_result.wall_time
         cpu_time = test_result.cpu_time
-        points = 0
+        points = point
 
         if status == 'C':
             status = check_one(result, correct)
@@ -43,11 +45,3 @@ def check_result(test_results, correct_results, submission_id):
         result_to_db.append(prepare_result)
 
     return result_to_db
-
-
-def add_results_to_db():
-    pass
-
-
-def change_submission_status_in_db():
-    pass
