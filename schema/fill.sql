1b079f7d5daa5c5edf5af23495963a79bee2ef2b
SashaKuprii <sasha2017.kuprii@gmail.com>
2020-03-23 17:08:05 +0200

    Added alias column to the tables task_descriptions and task_examples.

--- schema/fill.sql
+++ schema/fill.sql
@@ -49,6 +49,9 @@ INSERT INTO coreschema.tests (points, task_id) VALUES (10, 4);
 INSERT INTO coreschema.tests (points, task_id) VALUES (10, 4);
 INSERT INTO coreschema.tests (points, task_id) VALUES (10, 4);
 
-INSERT INTO coreschema.task_descriptions (task_id, main, input_format, output_format, explanation) VALUES (5, 'Вовчику дали особисте завдання додому – написати програму, яка визначатиме, чи містить задана на вході послідовність із **_N_** цілих чисел **_a<sub>1</sub>_**, **_a<sub>2</sub>_**, ..., **_a<sub>N</sub>_**, що по модулю не перевищують **_2<sup>31</sup>-1_**, дві підмножини з однаковою сумою, які не перетинаються. Зрозуміло, що підмножини не порожні.', 'Перший рядок містить натуральне число **_N (N <= 25)_**.  \nДругий рядок містить _**N**_ цілих **_a<sub>1</sub>_**, **_a<sub>2</sub>_**, ..., **_a<sub>N</sub>_**, які задають початкову множину.', 'Виведіть **_YES_**, якщо існують такі дві підмножини, інакше виведіть **_NO_**.', Null);
+INSERT INTO coreschema.task_descriptions (task_id, alias, main, input_format,
+output_format, explanation) VALUES (5, 'equal_sets', 'Вовчику дали особисте ' ||
+ 'завдання додому – написати програму, яка визначатиме, чи містить задана на вході послідовність із **_N_** цілих чисел **_a<sub>1</sub>_**, **_a<sub>2</sub>_**, ..., **_a<sub>N</sub>_**, що по модулю не перевищують **_2<sup>31</sup>-1_**, дві підмножини з однаковою сумою, які не перетинаються. Зрозуміло, що підмножини не порожні.', 'Перший рядок містить натуральне число **_N (N <= 25)_**.  \nДругий рядок містить _**N**_ цілих **_a<sub>1</sub>_**, **_a<sub>2</sub>_**, ..., **_a<sub>N</sub>_**, які задають початкову множину.', 'Виведіть **_YES_**, якщо існують такі дві підмножини, інакше виведіть **_NO_**.', Null);
 
-INSERT INTO coreschema.task_examples (task_id, input_data, output_data) VALUES (5, '6\n1 3 5 12 20 -1', 'YES');
+INSERT INTO coreschema.task_examples (task_id, alias, input_data, output_data)
+VALUES (5, 'equal_sets', '6\n1 3 5 12 20 -1', 'YES');
