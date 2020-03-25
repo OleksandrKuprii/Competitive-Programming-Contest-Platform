034d6f51855a50c6ddf894d5adcccc037e2f58c7
SashaKuprii <sasha2017.kuprii@gmail.com>
2020-03-25 11:25:31 +0200

    Refactored for flake8

--- backend/coreservices/toucan/coreservices/api.py
+++ backend/coreservices/toucan/coreservices/api.py
@@ -65,8 +65,25 @@ async def get_task_by_alias(request):
     return json_response(task_info)
 
 
+@routes.get('/submissions')
+async def get_submissions(request):
+    """Get list of submissions."""
+    params = request.rel_url.query
+
+    if list(params.values()).count('') or \
+            ('user_id' not in list(params.keys()) or
+             'number' not in list(params.keys()) or
+             'offset' not in list(params.keys())):
+        return Response(status=400)
+
+    user_id = int(params.get('user_id'))
+    number = int(params.get('number'))
+    offset = int(params.get('offset'))
+
+
 @routes.get('/result/{submission_id}')
 async def get_result(request):
+    """Get result."""
     submission_id = int(request.match_info['submission_id'])
 
     result = await submission_get_result(submission_id)
