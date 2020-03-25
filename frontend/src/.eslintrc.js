088ca38cb68ad40866b4f9c112d84372cd15c276
Igor Zashchelkin <igorzash@tutanota.com>
2020-03-25 15:53:50 +0200

    package.json cleanup

--- frontend/src/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['airbnb-typescript'],
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-  rules: {
-    "no-unused-vars":  ["error", { "argsIgnorePattern": "^_" }]
-  }
-};
