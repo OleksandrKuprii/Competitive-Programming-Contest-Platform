0f207bdfefa8a1d8096a9f8cbdb441f8dcb10af5
Igor Zashchelkin <igorzash@tutanota.com>
2020-03-31 21:43:58 +0300

    fix pipeline failure

--- backend/checker/setup.py
+++ backend/checker/setup.py
@@ -6,4 +6,7 @@ microlib_name = 'toucan.checker'
 setup(name=microlib_name,
       py_modules=['toucan.checker'],
       namespace_packages=['toucan'],
-      install_requires=['toucan.database', 'toucan.storage', 'aiohttp', 'aiohttp_cors'])
+      install_requires=['toucan.database',
+                        'toucan.storage',
+                        'aiohttp',
+                        'aiohttp_cors'])
