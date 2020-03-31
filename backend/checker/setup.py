8d3842b7c9f271b3fd1d2f0616d797447f219daa
Igor Zashchelkin <igorzash@tutanota.com>
2020-03-31 20:21:45 +0300

    Added requirements to setup.py

--- backend/checker/setup.py
+++ backend/checker/setup.py
@@ -6,4 +6,4 @@ microlib_name = 'toucan.checker'
 setup(name=microlib_name,
       py_modules=['toucan.checker'],
       namespace_packages=['toucan'],
-      install_requires=['toucan.database', 'toucan.storage'])
+      install_requires=['toucan.database', 'toucan.storage', 'aiohttp', 'aiohttp_cors'])
--- backend/task/setup.py
+++ backend/task/setup.py
@@ -6,4 +6,4 @@ microlib_name = 'toucan.task'
 setup(name=microlib_name,
       py_modules=['toucan.task'],
       namespace_packages=['toucan'],
-      install_requires=['toucan.dataclass', 'toucan.database'])
+      install_requires=['toucan.dataclass', 'toucan.database', 'markdown2'])
