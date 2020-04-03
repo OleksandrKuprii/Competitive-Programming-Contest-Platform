f009c194e67776fd97261cf93454bb0ddbef918f
Igor Zashchelkin <igorzash@tutanota.com>
2020-04-04 01:16:34 +0300

    Basic profile info

--- frontend/src/routes/HomePage.tsx
+++ frontend/src/routes/HomePage.tsx
@@ -1,19 +1,32 @@
 import * as React from 'react';
 import { Button, Jumbotron } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import ProfileInfo from '../components/ProfileInfo';
 
 const Homepage = () => {
   const { t } = useTranslation();
 
   return (
     <>
-      <Jumbotron>
-        <p className="h1 m-0">{t('homepage.welcome')}</p>
-        <p className="subtitle">{t('homepage.description')}</p>
-        <p>
-          <Button variant="primary">{t('learnMore')}</Button>
-        </p>
-      </Jumbotron>
+      <Row>
+        <Col>
+          <Jumbotron className="p-4">
+            <p className="h1 m-0">{t('homepage.welcome')}</p>
+            <p className="subtitle">{t('homepage.description')}</p>
+            <p>
+              <Button variant="primary">{t('learnMore')}</Button>
+            </p>
+          </Jumbotron>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <ProfileInfo />
+        </Col>
+      </Row>
     </>
   );
 };
