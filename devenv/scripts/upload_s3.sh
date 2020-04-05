d5f2ae9657a6d0dd03fd8f0abc2793ebd01270c2
SashaKuprii <sasha2017.kuprii@gmail.com>
2020-04-05 22:48:54 +0300

    Deleted extra files

--- devenv/scripts/upload_s3.sh
+++ devenv/scripts/upload_s3.sh
@@ -2,5 +2,3 @@
 set -e
 
 aws s3 cp "devenv/s3/tests/" "s3://tests/" --recursive --endpoint-url=$S3_ENDPOINT
-
-aws s3 cp "devenv/s3/submissions/" "s3://submissions/" --recursive --endpoint-url=$S3_ENDPOINT
