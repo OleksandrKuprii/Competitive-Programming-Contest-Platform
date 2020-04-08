2b1158ff780f5ff5add84ac9566d79e7522b6bb6
SashaKuprii <sasha2017.kuprii@gmail.com>
2020-04-08 17:40:46 +0300

    Writen a lot of docstrings and comments.

--- backend/checker/toucan/checker.py
+++ backend/checker/toucan/checker.py
@@ -1,38 +1,52 @@
 """Checks user submission program output."""
-import typing
+from typing import Callable, Generator, Iterable, List
 
 from toucan import database
 from toucan import storage
 from toucan.dataclass import ResultToChecker, ResultToDB, TestResult
 
 
-async def process_result_to_checker(result_to_checker: ResultToChecker):
-    """Check result_to_checker and update corresponding rows in database."""
+async def process_result_to_checker(result_to_checker: ResultToChecker
+                                    ) -> None:
+    """Check result_to_checker and update corresponding rows in database.
+
+    Parameters
+    ----------
+    result_to_checker: ResultToChecker
+        The ResultToChecker dataclass
+    """
     submission_id = result_to_checker.submission_id
 
+    # Getting test_results from result_to_checker and writing to list
     test_ids = [x.test_id for x in result_to_checker.test_results]
 
+    # Getting correct results from storage
     correct_results = storage.get_correct_results(test_ids)
 
+    # Getting points for each test from database
     points = await database.get_points_for_tests(test_ids)
 
+    # Checking results
     results_to_db = (
         result_to_db_without_submission_id(submission_id)
         for result_to_db_without_submission_id in check_test_results(
             result_to_checker.test_results, correct_results, points))
 
+    # Adding results to database
     await database.add_results_to_db(results_to_db)
 
+    # Calling database to update task bests
     await database.update_task_bests(submission_id)
 
+    # Changing submission status in database to 'Completed'
     await database.change_submission_status(submission_id, 'Completed')
 
 
 def check_test_results(
-        test_results: typing.List[TestResult],
-        correct_results: typing.List[str],
-        points_list: typing.List[int]
-) -> typing.Generator[typing.Callable[[int], ResultToDB], None, None]:
+        test_results: List[TestResult],
+        correct_results: Iterable[str],
+        points_list: List[int]
+) -> Generator[Callable[[int], ResultToDB], None, None]:
     """Compare correct results with given, calculate points.
 
     Returns anonymous ResultToDB instances -
