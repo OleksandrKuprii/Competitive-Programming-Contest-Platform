705eaf203c545a9d7ac8874d4c74de84bb3d3c6e
Igor Zashchelkin <igorzash@tutanota.com>
2020-04-12 18:56:40 +0300

    Refactored backend structure.

--- backend/storage/setup.py
+++
@@ -1,9 +0,0 @@
-"""setup.py for storage."""
-from setuptools import setup
-
-microlib_name = 'toucan.storage'
-
-setup(name=microlib_name,
-      py_modules=['toucan.storage'],
-      namespace_packages=['toucan'],
-      install_requires=['boto3', 'toucan.dataclass'])
--- backend/task/setup.py
+++
@@ -1,9 +0,0 @@
-"""setup.py for task."""
-from setuptools import setup
-
-microlib_name = 'toucan.task'
-
-setup(name=microlib_name,
-      py_modules=['toucan.task'],
-      namespace_packages=['toucan'],
-      install_requires=['toucan.dataclass', 'toucan.database', 'markdown2'])
