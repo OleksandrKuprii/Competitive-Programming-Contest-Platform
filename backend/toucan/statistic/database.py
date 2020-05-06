de87c397dbf0db98ab5c24a556b8c59e0c610171
SashaKuprii <sasha2017.kuprii@gmail.com>
2020-05-06 22:43:29 +0300

    Functions to update public tasks rating.

--- backend/toucan/statistic/database.py
+++ backend/toucan/statistic/database.py
@@ -72,3 +72,93 @@ async def get_data_task(task_id: int, conn: Connection) -> dict:
     else:
         # Else - the statistic is not provided, so function returns empty data
         return {'full': 0, 'partial': 0, 'zero': 0}
+
+
+async def update_public_task_rating(submission_id: int, conn: Connection):
+    """Update public task result rating by submission id.
+
+    From submission id function gets all information: user id, task id, amount of points. So it
+    does not need any other parameters.
+
+    Parameters
+    ----------
+    submission_id: int
+        The id of the submission
+    conn: Connection
+        The connection to the database
+    """
+    async with conn.transaction():
+        fetch = await conn.fetchrow("""
+            SELECT difficulty, user_id
+            FROM coreschema.submissions
+            JOIN coreschema.tasks
+            ON tasks.id = task_id
+            WHERE submissions.id = $1
+        """, submission_id)
+
+        difficulty = fetch['difficulty']
+        user_id = fetch['user_id']
+
+        fetch = await conn.fetchrow("""
+            SELECT *
+            FROM coreschema.rating
+            WHERE user_id = $1
+        """, user_id)
+
+        if fetch is None:
+            await conn.execute("""
+                INSERT INTO coreschema.rating (user_id, public_task_rating) VALUES ($1, $2)
+            """, user_id, difficulty)
+        else:
+            await conn.execute("""
+                UPDATE coreschema.rating
+                SET public_task_rating = public_task_rating + $1
+                WHERE user_id = $2
+            """, difficulty, user_id)
+
+
+async def check_necessity_of_updating_rating(submission_id: int, conn: Connection) -> bool:
+    """Check if there is necessity of updating public task rating.
+
+    By submission id this function compares previous best number of points for this task and
+    amount of points for this submission. If current is 100 and it is greater than previous
+    returns True, else - False.
+
+    Parameters
+     ----------
+     submission_id: int
+        The id of the submission
+     conn: Connection
+        The connection to the database
+
+    Returns
+    -------
+    _: bool
+        The necessity of updating
+    """
+    async with conn.transaction():
+        previous_points = await conn.fetchval("""
+            SELECT sum(results.points)
+            FROM coreschema.submissions
+            JOIN coreschema.task_bests
+            ON task_bests.task_id = submissions.task_id AND
+                task_bests.user_id = submissions.user_id
+            JOIN coreschema.results
+            ON results.submission_id = task_bests.submission_id
+            WHERE submissions.id = $1
+            GROUP BY results.submission_id
+        """, submission_id)
+
+        current_points = await conn.fetchval("""
+            SELECT SUM(points)
+            FROM coreschema.results
+            WHERE submission_id = $1
+            GROUP BY submission_id
+        """, submission_id)
+
+        if previous_points is None:
+            if current_points == 100:
+                return True
+            return False
+
+        return current_points > previous_points and current_points == 100
--- backend/toucan/statistic/statistic.py
+++ backend/toucan/statistic/statistic.py
@@ -14,3 +14,18 @@ async def get_data_task_with_lang(task_id: int, conn: Connection) \
 async def get_data_task(task_id: int, conn: Connection):
     """Call function of the same name from database module."""
     return await database.get_data_task(task_id, conn)
+
+
+async def update_public_task_rating(submission_id: int, conn: Connection):
+    """Update public task result rating by submission id.
+
+    Parameters
+     ----------
+     submission_id: int
+        The id of the submission
+     conn: Connection
+        The connection to the database
+    """
+    if await database.check_necessity_of_updating_rating(submission_id, conn):
+
+        await database.update_public_task_rating(submission_id, conn)
